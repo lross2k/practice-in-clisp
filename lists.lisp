808d635f11e832f715294f1656112ae53f838edf
lross2k <luisross2000@outlook.com>
2022-08-31 23:38:04 -0600

    Some more list functions

--- lists.lisp
+++ lists.lisp
@@ -14,3 +14,23 @@
 (setf (nth 2 list1) "a")
 (print list1)
 
+; Simple function to test map
+(defun map_test (n)
+  (print (/ n 2)))
+
+; Using map for simple addition
+(mapcar #'map_test (list 1 2 3 4 5))
+
+; Using reduce function
+(print (reduce #'- (list 1 3 5 7 9 11)))
+
+; Using the standard sort function
+(print (sort (list 9 2 6 2 9 1 8 4 5) #'<))
+
+; Destructuring list
+(destructuring-bind (first third &rest all)
+         (list 1 2 3 4 5 6 7)
+         (print first)
+         (print third)
+         (print all))
+
